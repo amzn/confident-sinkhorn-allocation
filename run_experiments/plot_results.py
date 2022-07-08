8eb91a8dd6d59d8a122bbdc2f2fca99bc72d24fa
vutngn <vutngn@amazon.com>
2022-07-08 10:16:49 +0930

    .

--- run_experiments/plot_results.py
+++ run_experiments/plot_results.py
@@ -30,8 +30,16 @@ save_dir = 'results_output' # path to the folder store the results
 out_file='' 
 numTrials=20 # number of repeated trials
 numIters=5 # number of used pseudo-iterations
-dataset_name='German-credit' #segment_2310_20 | wdbc_569_31 | analcatdata_authorship | synthetic_control_6c | \
+
+#====================================================================
+# list of datasets 
+#segment_2310_20 | wdbc_569_31 | analcatdata_authorship | synthetic_control_6c | \
         #German-credit |  madelon_no | dna_no | agaricus-lepiota | breast_cancer | digits | emotions
+dataset_name='wdbc_569_31' 
+
+
+
+
 list_algorithms=['Pseudo_Labeling','FlexMatch','UPS','CSA'] # list of algorithms to be plotted
 
 # the following parameters to be used to load the correct paths
--- run_experiments/run_csa.py
+++ run_experiments/run_csa.py
@@ -103,7 +103,7 @@ if __name__ == '__main__':
     parser.add_argument('--numTrials', type=int, default=20, help ='number of Trials (Repeated Experiments)' )
     parser.add_argument('--numXGBs', type=int, default=10, help ='number of XGB models, M=?' )
     parser.add_argument('--confidence_choice', type=str, default='ttest', help ='confidence choices: ttest | variance | entropy | None' )
-    parser.add_argument('--dataset_name', type=str, default='German-credit', help='segment_2310_20 | wdbc_569_31 | analcatdata_authorship | synthetic_control_6c | \
+    parser.add_argument('--dataset_name', type=str, default='wdbc_569_31', help='segment_2310_20 | wdbc_569_31 | analcatdata_authorship | synthetic_control_6c | \
         German-credit |  madelon_no | dna_no | agaricus-lepiota | breast_cancer | digits | yeast | emotions')
     parser.add_argument('--verbose', type=str, default='True', help='verbose True or False')
     parser.add_argument('--output_filename', type=str, default='', help='name of output files')
